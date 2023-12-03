0b27eac22f7250abe5224f68852a325015831d5b
Anjila Tripathi <anjila.tripathi@gmail.com>
2023-12-03 21:36:19 +0545

    Ways to initialize object

---
+++ InitObjClass/src/InitilizeObj.java
@@ -0,0 +1,51 @@
+//////There are three ways of initailzing object in class. They are
+////
+////
+//////1. By Reference Variable
+////class InitilizeObj
+////{
+////    String name;
+////    int age;
+////    
+////   
+////        public static void main (String[] args)
+////        {
+////            InitilizeObj p = new InitilizeObj();
+////            p.name="Anjila";
+////                    p.age=22;
+////                    System.out.println("My name is:"+p.name+" "+"My age is:"+p.age);
+////            
+////       
+////    }
+////}
+//
+//
+//
+//////2.By method
+//class InitilizeObj
+//{
+//    String name;
+//    int age;
+//    
+//    public void input(String n, int a)
+//    {
+//        name = n;
+//        age = a;
+//    }
+//    public void display()
+//    {
+//        System.out.println("Name:"+name+" "+ "Age:"+ age);
+// 
+//   }
+//    
+//    public static void main(String[] args)
+//    {
+//        InitilizeObj a =new InitilizeObj();
+//        a.input("Anjila", 22);
+//        a.display();
+//    }
+//}
+//
+
+
+//3. By Constructor
