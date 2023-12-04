d944e383db3a9847d35dfaa22bd3620d31fb8930
Anjila Tripathi <anjila.tripathi@gmail.com>
2023-12-04 14:13:38 +0545

    Swing

---
+++ SwingExample1/src/Sample.java
@@ -0,0 +1,15 @@
+//Basic Sample Code for Java Swing
+
+import javax.swing.*;
+
+public class Sample
+{
+    public static void main (String[] args)
+    {
+        JFrame f = new JFrame();
+        f.setTitle("Sample");
+        f.setSize(500,500); 
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
