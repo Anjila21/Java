63b1816640799f9b265f08adb642c0220cbe0381
Anjila Tripathi <anjila.tripathi@gmail.com>
2023-12-28 16:05:00 +0545

    Types of Layout Manager

--- BoxLayout/src/BoxLayout1.java
+++ BoxLayout/src/BoxLayout1.java
@@ -1,20 +1,18 @@
 import javax.swing.*;
 import java.awt.Container;
-import java.awt.GridLayout;
+import javax.swing.BoxLayout;
 
-public class GridLayout11
+public class BoxLayout1
 {
     public static void main(String[] args)
     {
-        JFrame g = new JFrame("Grid Layout");
-        g.setSize(500,500);
-        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        g.setLocationRelativeTo(null);
-        
-        Container c = g.getContentPane();
-        
-        GridLayout gridLayout =new GridLayout();
-        c.setLayout(gridLayout);
+        JFrame j = new JFrame("BoxLayout");
+        j.setSize(500,500);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setLocationRelativeTo(null);
+        Container c = j.getContentPane();
+        BoxLayout boxLayout = new BoxLayout(c,BoxLayout.X_AXIS);
+        c.setLayout(boxLayout);
         
         JButton b1 = new JButton("Button 1 ");
         JButton b2 = new JButton("Button 2 ");
@@ -22,13 +20,14 @@ public class GridLayout11
           JButton b4 = new JButton("Button 4 ");
            JButton b5 = new JButton("Button 5 ");
            
+              
            c.add(b1);
             c.add(b2);
              c.add(b3);
               c.add(b4);
                c.add(b5);
-               
-            g.setVisible(true);
         
+        
+        j.setVisible(true);
     }
 }
